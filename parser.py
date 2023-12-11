296210f99b225a83ffc7bf838b4fdc29f5ec5eba
Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>
2023-12-11 12:14:54 +0530

    Updated
    Signed-off-by: Mukesh Kumar Chaurasiya <mchauras@linux.ibm.com>

--- parser.py
+++ parser.py
@@ -4,8 +4,19 @@ import perf_stat
 from perf_stat import PerfStatParser
 import os
 
+def parse_ebizzy_data_get_records(parser):
+    count = 0
+    record = 0
+    for stat in parser:
+        count += 1
+        for line in stat.extra_data.split("\n"):
+            if "records" in line:
+                record += int(line.split(" ")[0])
+    return str(record/count)
+                
+
 def get_avg_in_dir(path, output):
-    print(path)
+    # print(path)
     parsed = False
     parser = []
     for entry in os.scandir(path):
@@ -17,14 +28,13 @@ def get_avg_in_dir(path, output):
     if parsed:
         avg = perf_stat.take_average_of_data(parser)
         time_threads = path.split('/')
-        print(time_threads)
-        output.write("\n" + time_threads[-2] + "," + time_threads[-1] + "," + avg.get_csv())
+        output.write("\n" + time_threads[-2] + "," + time_threads[-1] + "," + avg.get_csv() + parse_ebizzy_data_get_records(parser))
         # print(avg.get_csv())
 
 result = "data.csv"
 try:
     with open(result, 'w') as file:
-        file.write("base_slice," + "threads," + perf_stat.get_csv_header())
+        file.write("base_slice," + "threads," + perf_stat.get_csv_header() + "records")
         get_avg_in_dir("result", file)
         file.close()
 except Exception as e:
---
+++ plot_graph.py
@@ -0,0 +1,39 @@
+#! /bin/python3
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+arguments = sys.argv[1:]
+if len(arguments) < 2:
+    print(f"Usage: {sys.argv[0]} <Coloumn1> <Coloumn2>")
+    print("Optional:")
+    print("3rd and 4th argument are for data control in which you can tell which values to consider")
+    print("Coloumn Name", "Value to be equal")
+    print("Optional 5th Argunment to append to title of graph")
+    exit(-1)
+
+file_path = 'data.csv'
+
+# Select columns for the graph
+x_column = sys.argv[1]
+y_column = sys.argv[2]
+
+for i in range(104, 2081, 104):
+# Read the CSV file into a pandas DataFrame
+    data = pd.read_csv(file_path)
+# data = data[data[sys.argv[3]] == sys.argv[4]]
+    data = data[data['threads'] == i]
+# Get the column names from the CSV file
+    column_names = data.columns.tolist()
+    print("Available columns:", column_names)
+
+# Plot the graph
+    plt.figure(figsize=(8, 6))  # Adjust figure size if needed
+    plt.plot(data[x_column], data[y_column])
+    plt.title(f"{x_column} vs {y_column} {i} Threads")
+    plt.xlabel(x_column)
+    plt.ylabel(y_column)
+    plt.grid(True)
+    # plt.show()
+    plt.savefig(f"graph/bs-vs-{sys.argv[2]}-{i}T.png")
